e6626dde7ce3f0830dc97c8767f688da2ec2dfcf
ArturoRoedell <arturoquddus@gmail.com>
2023-06-26 18:30:52 -0400

    Add project files.

---
+++ VSIXProject1004/ToolWindows/ToolWindow1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VSIXProject1004
+{
+	public class ToolWindow1 : BaseToolWindow<ToolWindow1>
+	{
+		public override string GetTitle(int toolWindowId) => "ToolWindow1";
+
+		public override Type PaneType => typeof(Pane);
+
+		public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+		{
+			return Task.FromResult<FrameworkElement>(new ToolWindow1Control());
+		}
+
+		[Guid("5d93a9e3-5d1b-478a-9805-28761bfb03b0")]
+		internal class Pane : ToolkitToolWindowPane
+		{
+			public Pane()
+			{
+				BitmapImageMoniker = KnownMonikers.ToolWindow;
+			}
+		}
+	}
+}
---
+++ VSIXProject1004/ToolWindows/ToolWindow1Control.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace VSIXProject1004
+{
+	public partial class ToolWindow1Control : UserControl
+	{
+		public ToolWindow1Control()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, RoutedEventArgs e)
+		{
+			VS.MessageBox.Show("ToolWindow1Control", "Button clicked");
+		}
+	}
+}
