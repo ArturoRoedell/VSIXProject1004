6d5096a4625af6ad972a8b3f4595d89aff97076a
ArturoRoedell <arturoquddus@gmail.com>
2023-07-13 13:55:42 -0400

    AddImage to xaml

--- VSIXProject1004/ToolWindow1Control.xaml.cs
+++ VSIXProject1004/ToolWindow1Control.xaml.cs
@@ -2,6 +2,26 @@
 using System.Windows;
 using System.Windows.Controls;
 
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BackEndSharedLibrary;
+using BackEndSharedLibrary.VizFuncFileDataAndHelpers;
+using BackEndSharedLibrary.OtherMiscFuncs.BaseFuncs;
+using Path = System.Windows.Shapes.Path;
+using static BackEndSharedLibrary.OtherMiscFuncs.BaseFuncs.PrintClass;
+using static BackEndSharedLibrary.VizFuncFileDataAndHelpers.VizFuncFileData;
+using System.Threading.Tasks;
+
+
 namespace VSIXProject1004
 {
 	/// <summary>
@@ -27,9 +47,57 @@ namespace VSIXProject1004
 		private void button1_Click(object sender, RoutedEventArgs e)
 		{
 			//string testthis = FileContentsClass.ReadFileContents(ActiveCodeFilePath);;
+			TestImageWindow();
+			
 			MessageBox.Show(
 				string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
 				"ToolWindow1");
 		}
+		
+		private async Task TestImageWindow()
+		{
+			string ActiveCodeFilePath = @"C:\Users\ARTURO 001\source\repos\001ScratchCode\001ReadCodeFile\TestCodeFile.cs";
+			string ActiveCode = FileContentsClass.ReadFileContents(ActiveCodeFilePath);
+			
+			
+			VizFuncFileData Mc = new VizFuncFileData(ActiveCode);
+			Mc.CodeFullFilePath = ActiveCodeFilePath;
+			PascalSearch.RefineResultOptions refineOpt = new PascalSearch.RefineResultOptions();
+			Mc._RefineResultForPascal = refineOpt;
+			await Mc.AllocatePascalSearchListAndGetUrls();
+			await MainMethods.SubMethods.DownloadAllImagesFourEach(Mc);
+			foreach ( var newPascalSearch in Mc.PascalSearchList)
+			{
+				printl();
+				printl("############################################");
+				printl("NAME: " + newPascalSearch.MethodClassName);
+				printl("############################################");
+				printl();
+				for (int i = 0; i < newPascalSearch.FinalWordCount; i++)
+				{
+					printl(newPascalSearch.ImageResultsUrl[i,0]);
+				}
+			}
+			foreach (var PairList in Mc.LineNumberPascalNamePairList)
+			{
+				printl(PairList.pascalNames);
+				printl("Line_NUmber: #" + PairList.lineNumber.ToString());
+				printl("----------------------------------------------------------");
+			}
+		
+			MainMethods.SubMethods.SaveAllVizfuncDataToFile(Mc);
+			
+			
+			
+			// this.Image01.Source = new BitmapImage(
+			// 	new Uri(fullPath, UriKind.RelativeOrAbsolute));
+			
+			
+			
+		}
+		
 	}
+	
+
+	
 }
